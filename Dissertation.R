64780756685768b3a25fcf85a8be5056f77fdcb2
Ayomikun7 <102880886+Ayomikun7@users.noreply.github.com>
2023-08-08 01:46:58 +0100

    Update Dissertation.R

--- Dissertation.R
+++ Dissertation.R
@@ -1,97 +1,169 @@
-install.packages("readr")
-library(readr)
-
-survdata <- read_tsv("prad_mcspc_mskcc_2020_clinical_data.tsv")
-survdata1 <- read_tsv('prad_msk_stopsack_2021_clinical_data.tsv')
-
-head(survdata)
-summary(survdata)
+## Data Wrangling: Stopsack PRAD
+## Lewis Quayle, PhD
+## 2023-07-27
 
 
+# setup -------------------------------------------------------------------
 
-install.packages("dplyr")
-library(dplyr)
-survdataMerge <- inner_join(survdata, survdata1, by = "Patient ID")
-
-
-sum(is.na(survdataMerge$`Race Category.y`))
-colSums(is.na(survdataMerge))
-
-sum(is.na(survdataMerge$`Race Category.y`))
-sum(is.na(survdataMerge$`Survival Status`))
-
-# Assuming your dataset is named "survdataMerge" and the variable you want to analyze is "Race Category.y"
-unique_categories <- unique(survdataMerge$`Race Category.y`)
-
-num_categories <- length(unique_categories)
-print(num_categories)
-
-#Using dplyr package's select function:
-survdataMerge1 <- survdataMerge %>% select(`Patient ID`,`Age at Diagnosis`,`Age at Diagnosis`, `Androgen Deprivation Therapy (ADT)`, `Timing of metastases`,`Time from sample to castration resistance [months]`,`Overall Survival since Sample Collection (Months)`,`Fraction Genome Altered.y`, `Race Category.y`,`Prostate-specific antigen.y`,Smoking,`Sample Type`,`TMB (nonsynonymous).y`,`Survival Status`,`Patient's Vital Status` )
-sum(is.na(survdataMerge1))
-colSums(is.na(survdataMerge1))
-
-survdataMerge1
-
-survdataMerge1 <- survdataMerge1 %>%
-  mutate(`Race Category.y` = as.numeric(factor(`Race Category.y`, levels = unique(`Race Category.y`))) - 1)
-
-# Encode "survival status" into 0 and 1
-survdataMerge1 <- survdataMerge1 %>%
-  mutate(`Survival Status` = recode(`Survival Status`, "Dead" = 1, .default = 0))
-
-
-install.packages(c("survival", "survminer"))
+# load libraries
+library("tidyverse")
+library("here")
+library("janitor")
 library("survival")
 library("survminer")
 
-install.packages("ggplot2")
-library('ggplot2')
+# directory hierarchy
+project_dir <- here()
+in_dir <- file.path(project_dir, "data")
+out_dir <- file.path(project_dir, "results", "proc_data")
 
 
-data_type <- class(survdataMerge1$`Race Category.y`)
-print(data_type)
-status_data_type <- class(survdataMerge1$`Survival Status`)
-print(status_data_type)
+# read data ---------------------------------------------------------------
+
+sm_424 <- read_tsv(file = file.path(in_dir, "prad_mcspc_mskcc_2020_clinical_data.tsv"))
+lg_2069 <- read_delim(file = file.path(in_dir, "prad_msk_stopsack_2021_clinical_data.tsv"))
+
+
+# clean and join data -----------------------------------------------------
+
+# clean variable names
+sm_424_cln <- 
+  sm_424 %>% 
+  janitor::clean_names()
+
+lg_2069_cln <- 
+  lg_2069 %>% 
+  janitor::clean_names()
+
+# join data sets
+joined_df <- 
+  sm_424_cln %>% 
+  left_join(lg_2069_cln, by = "patient_id") %>% 
+  mutate(race_category = coalesce(race_category.x, race_category.y)) # fills in NA by merging two race_category cols
+
+category_counts <- table(joined_df_cln$race_category)
+print(category_counts)
 
 
 
-# Assuming dataset is named "survdataMerge1" and the "Survival Status" column is named "Survival_Status"
-# Convert character values to logical (TRUE/FALSE)
-survdataMerge1$`Survival Status` <- survdataMerge1$`Survival Status` == "Alive"  # Change "Event" to the appropriate event indicator value
+# there are 24 non-concordant patient IDs so this is as good as it gets - could filter later to remove missingness
+joined_df %>%
+  select(starts_with("race_")) %>% 
+  summarise(across(everything(), ~ sum(is.na(.))))
 
-summary(survdataMerge1)
-
-survdataMerge1$`Race Category.y` <- factor(survdataMerge1$`Race Category.y`, levels = unique(survdataMerge1$`Race Category.y`))
-
-# We want to compute the survival probability by Race Category
-surv_obj <- Surv(time =survdataMerge1$`Overall Survival since Sample Collection (Months)`, event = survdataMerge1$`Survival Status`)
-
-
-
-survdataMerge1
-
-
-
-survdataMerge2 <- survdataMerge1 %>% select(`Patient ID`,`Overall Survival since Sample Collection (Months)`,`Race Category.y`,Smoking,`Survival Status`,`Patient's Vital Status` )
-
-# Fit the survival model
-surv_obj <- Surv(time =survdataMerge2$`Overall Survival since Sample Collection (Months)`, event = survdataMerge2$`Survival Status`)
-
-surv_fit <- survfit(surv_obj ~ `Race Category.y`, data = survdataMerge2)
+# clean joined data frame: select vars of interest/remove duplicate vars, re-code chr to fct/lgcl etc., filter rows
+joined_df_cln <- 
+  joined_df %>% 
+  select(everything()) %>% # replace everything() with only vars you want to keep
+  mutate(
+    across(where(is_character), as_factor),
+    survival_status = ifelse(str_detect(survival_status, fixed("dead", ignore_case = TRUE)), yes = 0, no = 1)
+    
+    # could include cleaning of variables to homogenise the format here before converting all to factor
+    # may need to re-code some vars that should not be fct here
+    
+  ) %>% 
+  filter(!is.na(race_category))
 
 
-fit <- survfit(Surv(`Overall Survival since Sample Collection (Months)`, `Survival Status`) ~ `Race Category.y`, data = survdataMerge1)
+# fit survival model ------------------------------------------------------
 
+fit <- 
+  survfit(
+    Surv(overall_survival_since_sample_collection_months, survival_status) ~ race_category,
+    data = joined_df_cln
+  )
 
 print(fit)
 
+# Summary of survival curves
+summary(fit)
+# Access to the sort summary table
+summary(fit)$table
 
-# Assuming your dataset is named "survdataMerge1"
-print(class(survdataMerge1$`Overall Survival since Sample Collection (Months)`))
-print(class(survdataMerge1$`Survival Status`))
-print(class(survdataMerge1$`Race Category.y`))
-# Assuming your dataset is named "survdataMerge1"
-print(names(survdataMerge1))
+joined_df_cln_tab <- data.frame(time = fit$time,
+                                n.risk = fit$n.risk,
+                                n.event = fit$n.event,
+                                n.censor = fit$n.censor,
+                                surv = fit$surv,
+                                upper = fit$upper,
+                                lower = fit$lower
+)
+head(joined_df_cln_tab)
+
+
+# Change color, linetype by strata, risk.table color by strata
+ggsurvplot(fit,
+           pval = TRUE,
+           conf.int = TRUE,
+           risk.table = TRUE,
+           risk.table.col = "strata",
+           linetype = "strata",
+           surv.median.line = "hv",
+           ggtheme = theme_bw())
+
+
+# Get unique categories
+categories <- unique(joined_df_cln$race_category)
+print(categories)
+
+
+ggsurvplot(
+  fit,                     # survfit object with calculated statistics.
+  pval = TRUE,             # show p-value of log-rank test.
+  conf.int = TRUE,         # show confidence intervals for 
+  # point estimaes of survival curves.
+  conf.int.style = "step",  # customize style of confidence intervals
+  xlab = "Time in months",   # customize X axis label.
+  break.time.by = 20,     # break X axis in time intervals by 20.
+  ggtheme = theme_light(), # customize plot and risk table with a theme.
+  risk.table = "abs_pct",  # absolute number and percentage at risk.
+  risk.table.y.text.col = T,# colour risk table text annotations.
+  risk.table.y.text = FALSE,# show bars instead of names in text annotations
+  # in legend of risk table.
+  ncensor.plot = TRUE,      # plot the number of censored subjects at time t
+  surv.median.line = "hv",  # add the median survival pointer.
+  legend.labs = 
+    c("White", "Black or African American", "Other", "Black", "Asian"),    # change legend labels.
+
+)
+
+summary(fit)$table
+
+#he survival curves can be shorten using the argument xlim
+ggsurvplot(fit,
+           conf.int = TRUE,
+           risk.table.col = "strata", # Change risk table color by groups
+           ggtheme = theme_bw(), # Change ggplot2 theme
+           xlim = c(0, 80))
+
+#to plot cumulative events
+ggsurvplot(fit,
+           conf.int = TRUE,
+           risk.table.col = "strata", # Change risk table color by groups
+           ggtheme = theme_bw(), # Change ggplot2 theme
+           fun = "event")
+
+#To plot cumulative hazard
+ggsurvplot(fit,
+           conf.int = TRUE,
+           risk.table.col = "strata", # Change risk table color by groups
+           ggtheme = theme_bw(), # Change ggplot2 theme
+           fun = "cumhaz")
+
+#Kaplan-Meier life table: summary of survival curves.......................
+summary(fit)
+
+#function surv_summary() [in survminer package] to get a summary of survival curves.
+res.sum <- surv_summary(fit)
+head(res.sum)
+
+attr(res.sum, "table")
+
+#Log-Rank test comparing survival curves: survdiff()
+surv_diff <- survdiff(Surv(overall_survival_since_sample_collection_months, survival_status) ~ race_category,
+                      data = joined_df_cln
+)
+surv_diff
 
 
